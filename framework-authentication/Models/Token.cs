02ff91a7adf9b6bf8a495f9629acd360726dfcd1
Jimmy Soussan <jimmy.soussan@ynov.com>
2020-02-20 12:48:24 +0100

    Token class skeleton

---
+++ framework-authentication/Models/Token.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace framework_authentication
+{
+    public class Token
+    {
+        private string token;
+        private DateTime expireDate;
+
+        public Token()
+        {
+
+        }
+
+        public bool isValid()
+        {
+            return true;
+        }
+
+        public bool delete()
+        {
+            return true;
+        }
+
+        public string getToken()
+        {
+            return "";
+        }
+
+    }
+}
