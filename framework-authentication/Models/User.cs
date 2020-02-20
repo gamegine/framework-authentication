e0d4d3df5b44b0efd115394d0e859b9e9d87b2cc
Nicolas jose Dos santos <nicolas.dossants@laposte.net>
2020-02-20 17:38:47 +0100

    models User

---
+++ framework-authentication/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace framework_authentication.Models
+{
+    public class User
+    {
+        private string id;
+        private List<Token> tokens;
+
+        public User()
+        {
+  
+        }
+
+        public bool login()
+        {
+            return true;
+        }
+
+        public bool logout(string token)
+        {
+            return true;
+        }
+        public bool islog(string token)
+        {
+            return true;
+        }
+    }
+}
