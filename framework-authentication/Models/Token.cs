e9443d2f34142ae65a91bea15f820a4475940214
gamegine <gamegine@protonmail.com>
2020-03-12 19:39:57 +0100

    init models

---
+++ framework-authentication/Models/Token.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace framework_authentication.Models
+{
+    public class Token
+    {
+        public string token { get; set; }
+        public Token() => throw new NotImplementedException();
+        public bool IsValid() => throw new NotImplementedException();
+    }
+}
---
+++ framework-authentication/Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace framework_authentication.Models
+{
+    public class Users
+    {
+        private int id;
+        public ICollection<Token> tokens { get; set; }
+        public Token login() => throw new NotImplementedException();
+        public bool logout() => throw new NotImplementedException();
+        public bool islog() => throw new NotImplementedException();
+    }
+}
