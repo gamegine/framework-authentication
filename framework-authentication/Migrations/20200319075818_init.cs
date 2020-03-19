5e677dae5a66e9eb0de776303051306938739b24
gamegine <gamegine@protonmail.com>
2020-03-19 09:14:29 +0100

    add migration init

---
+++ framework-authentication/Migrations/20200319075818_init.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace framework_authentication.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Token",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    token = table.Column<string>(nullable: true),
+                    Usersid = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Token", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Token_Users_Usersid",
+                        column: x => x.Usersid,
+                        principalTable: "Users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Token_Usersid",
+                table: "Token",
+                column: "Usersid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Token");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ framework-authentication/entity-framework.md
@@ -0,0 +1,21 @@
+﻿# entity-framework
+## migration
+https://docs.microsoft.com/fr-fr/aspnet/mvc/overview/getting-started/getting-started-with-ef-using-mvc/migrations-and-deployment-with-the-entity-framework-in-an-asp-net-mvc-application
+
+## Console du gestionnaire de package
+Dans le menu **Outils**, sélectionnez **Gestionnaire de package NuGet** > **Console du gestionnaire de package**
+À l’invite `PM>`, entrez les commandes suivantes :
+
+### ajouter une migration
+```
+add-migration <name>
+```
+
+### Mettre à jour la bdd avec une migration
+```
+Update-Database
+```
+#### Mettre à jour une migration spécifique
+```
+update-database -target <name>
+```
