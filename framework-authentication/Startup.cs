cddaf63625e5a83cff0e20cc8668031be67312a8
gamegine <gamegine@protonmail.com>
2020-03-27 13:11:22 +0100

    add swagger

--- framework-authentication/Startup.cs
+++ framework-authentication/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using framework_authentication.Data;
+using Microsoft.OpenApi.Models;
 
 namespace framework_authentication
 {
@@ -31,6 +32,12 @@ namespace framework_authentication
             services.AddControllersWithViews();
             services.AddDbContext<framework_authenticationContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("framework_authenticationContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +57,15 @@ namespace framework_authentication
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
